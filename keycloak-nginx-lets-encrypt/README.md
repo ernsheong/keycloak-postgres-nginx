41d3262cf2f03cee75456c62d60510cb745ba2eb
Jonathan ES Lin <ernsheong@gmail.com>
2017-09-08 00:02:53 +0800

    Add crontab for renewal

--- keycloak-nginx-lets-encrypt/README.md
+++ keycloak-nginx-lets-encrypt/README.md
@@ -1,1 +1,3 @@
 Credits to https://github.com/dklein-net/docker-keycloak-nginx for providing the original module.
+
+Provides Let's Encrypt support.
--- keycloak-nginx-lets-encrypt/entrypoint.sh
+++ keycloak-nginx-lets-encrypt/entrypoint.sh
@@ -10,10 +10,19 @@ if [ -n "$KEYCLOAK_HOST" ] && \
     sed -i s/__KEYCLOAK_PORT__/$KEYCLOAK_PORT/g /etc/nginx/conf.d/keycloak.conf
     sed -i s/__KEYCLOAK_DOMAIN__/$KEYCLOAK_DOMAIN/g /etc/nginx/conf.d/keycloak.conf
 
-    certbot certonly -n "${LE_OPTIONS}" --agree-tos --email "${LE_EMAIL}" --webroot -w /usr/share/nginx/html -d $KEYCLOAK_DOMAIN
+    certbot certonly -n "${LE_OPTIONS}" \
+        --agree-tos --email "${LE_EMAIL}" \
+        --webroot -w /usr/share/nginx/html -d $KEYCLOAK_DOMAIN \
+        --post-hook="service nginx reload"
 
     # Start nginx
     nginx -g "daemon off;"
+
+    # Install certificate renewing crontab
+    touch crontab.tmp \
+        && echo "37 2 * * * certbot renew ${LE_RENEW_OPTIONS}" > crontab.tmp \
+        && crontab crontab.tmp \
+        && rm -rf crontab.tmp
 else
     echo "ERROR: please provide KEYCLOAK_HOST, KEYCLOAK_PORT, KEYCLOAK_DOMAIN, LE_EMAIL"
 fi
