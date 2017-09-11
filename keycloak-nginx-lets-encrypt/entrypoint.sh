e9baef691b778512b4a3d0785d214b4e8bd3287d
Jonathan ES Lin <ernsheong@gmail.com>
2017-09-11 14:21:48 +0800

    Add post-hook for certbot renew

--- keycloak-nginx-lets-encrypt/entrypoint.sh
+++ keycloak-nginx-lets-encrypt/entrypoint.sh
@@ -42,7 +42,7 @@ if [ -n "$KEYCLOAK_HOST" ] && \
 
         # Install crontab for cert renewal
         touch crontab.tmp \
-            && echo "37 2 * * * certbot renew ${LE_RENEW_OPTIONS} && ${LE_RENEW_CRON_COMMAND} > /dev/null 2>&1" > crontab.tmp \
+            && echo "37 2 * * * certbot renew ${LE_RENEW_OPTIONS} --post-hook 'nginx -s reload' && ${LE_RENEW_CRON_COMMAND} > /dev/null 2>&1" > crontab.tmp \
             && crontab crontab.tmp \
             && rm -rf crontab.tmp
     ) &
